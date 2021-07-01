26d0380acc0dce98735f6633427442e8076204ad
Vector2588 <natkamol082@gmail.com>
2021-07-01 23:15:46 +0700

    question 6.7

--- main.js
+++ main.js
@@ -3,23 +3,31 @@ const app = Vue.createApp({
         return{
             product: 'Shoes',
             image: './assets/images/socks_green.jpg',
-            inStock: false,
+            inStock: true,
             onSale: true,
-            inventory: 100,
+            inventory: 10,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                {id: 2234, color: 'green', image: './assets/images/sock_green.jpg'},
-                {id: 2235, color: 'blue', image: './assets/images/sock_blue.jpg'}
+                {id: 2234, color: 'green', image: './assets/images/socks_green.jpg'},
+                {id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg'}
             ],
             cart: 0,
+            status: 'Available',
+            status2: 'Unavailable'
         }
     },
     methods: {
         addToCart() {
             this.cart += 1
+            this.inventory -= 1
         },
         updateImage(variantImage) {
             this.image = variantImage
+        },
+        removeFromCart() {
+            this.inventory += 1
+            this.cart -= 1
+            
         }
     }
 })
