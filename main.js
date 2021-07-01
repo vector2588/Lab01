ac32e6547c4d948838c3c6a151781b9cb9fbc901
Vector2588 <natkamol082@gmail.com>
2021-07-01 22:28:57 +0700

    question 5.3

--- main.js
+++ main.js
@@ -7,6 +7,10 @@ const app = Vue.createApp({
             onSale: true,
             inventory: 100,
             details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                {id: 2234, color: 'green'},
+                {id: 2235, color: 'blue'}
+            ],
             url: 'https://www.camt.cmu.ac.th/index.php/th/'
         }
     }
