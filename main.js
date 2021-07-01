d905c180b6ee9e07ac0c1c0ca8bd76513e41c3c3
Vector2588 <natkamol082@gmail.com>
2021-07-01 22:24:52 +0700

    question 5.2

--- main.js
+++ main.js
@@ -5,7 +5,8 @@ const app = Vue.createApp({
             image: './assets/images/socks_green.jpg',
             inStock: false,
             onSale: true,
-            inventory: 5,
+            inventory: 100,
+            details: ['50% cotton', '30% wool', '20% polyester'],
             url: 'https://www.camt.cmu.ac.th/index.php/th/'
         }
     }
